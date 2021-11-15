b56aae266c22b1a9c6e3f7c53ebbb57ebaad6c18
andej241 <andej241@newschool.edu>
2021-11-15 16:10:39 -0500

    ..

--- proto3/script/scriptscrolly.js
+++ proto3/script/scriptscrolly.js
@@ -11,12 +11,10 @@ document.addEventListener('DOMContentLoaded', async function () {
     console.log(numberSteps);
   
     scroller
-      .setup({
-        step: '.scroll-step',
-      })
+      .setup({step: '.scroll-step'})
+
+    
       .onStepEnter((response) => {
-        let desiredOpacity = response.element.getAttribute('data-size');
-  
   
         if (response.index == 0) {
         changeOpacity(".first", 1)
@@ -24,25 +22,24 @@ document.addEventListener('DOMContentLoaded', async function () {
   
         else if (response.index == 1) {
           changeOpacity(".second", 1)
-          }
+        }
   
         else if (response.index == 2) {
             changeOpacity(".third", 2)
-            }
+        }
   
         else if (response.index == 3) {
               changeOpacity(".fourth", 3)
-              }
+        }
   
         else if (response.index == 4) {
                 changeOpacity(".fifth", 4)
-                }
-      })
-      .onStepExit((response) => {
-        if (response.index == numberSteps - 1) {
-          changeFirstOpacity(400, 400);     
-          changeSecondOpacity(400, 400); 
         }
+      })
+
+
+      .onStepExit((response) => {
+        if (response.index == numberSteps - 1) {}
       });
       
   });
